3c30bd892ea7809ee1a48a1923853c66052c7175
Raumid <raumid98@gmail.com>
2025-04-06 16:34:45 -0600

    Pagination

--- Pacagroup.Ecommerce.Transversal.Common/Response.cs
+++ Pacagroup.Ecommerce.Transversal.Common/Response.cs
@@ -1,20 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using FluentValidation.Results;
-
-namespace Pacagroup.Ecommerce.Transversal.Common
+﻿namespace Pacagroup.Ecommerce.Transversal.Common
 {
-    public class Response<T>
+    public class Response<T> : ResponseGeneric<T>
     {
-        public T Data { get; set; }
-        public bool IsSuccess { get; set; }
-        public string Message { get; set; }
 
-        public IEnumerable<ValidationFailure> Errors { get; set; }
 
     }
 }
